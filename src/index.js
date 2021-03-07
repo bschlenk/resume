3d2d8bbea426683eb0dfe3f962153d812ae063bb
Brian Schlenker <bschlenk@umich.edu>
2021-03-06 22:15:49 -0500

    Fix failure if dist dir does not exist

--- src/index.js
+++ src/index.js
@@ -5,22 +5,25 @@ const dateFilter = require('nunjucks-date-filter');
 const pdf = require('html-pdf');
 const resume = require('./resume.json');
 
-const OUT_FILE = path.resolve(__dirname, '..', 'dist', 'resume.pdf');
+const TEMPLATE_DIR = path.resolve(__dirname, 'templates');
+const OUT_DIR = path.resolve(__dirname, '..', 'dist');
+const OUT_PDF = path.resolve(OUT_DIR, 'resume.pdf');
+const OUT_HTML = path.resolve(OUT_DIR, 'resume.html');
 
 const theme = {
   styles: [path.resolve(__dirname, 'style.css')],
 };
 
-const env = nunjucks.configure(path.resolve(__dirname, 'templates'));
+const env = nunjucks.configure(TEMPLATE_DIR, { trimBlocks: true });
 env.addFilter('date', dateFilter);
 env.addGlobal('includeFile', src => fs.readFileSync(src));
 
 const html = env.render('index.html', { ...resume, theme });
-fs.writeFileSync(path.resolve(__dirname, '..', 'dist', 'resume.html'), html);
-console.log(html);
+fs.mkdirSync(OUT_DIR, { recursive: true });
+fs.writeFileSync(OUT_HTML, html);
 pdf.create(html, {
   format: 'Letter',
-}).toFile(OUT_FILE, (err) => {
+}).toFile(OUT_PDF, (err) => {
   if (err) {
     console.error(err);
   }
