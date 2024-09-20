79b360e914569f3efb1d48198d72f2ddcf17bc6e
Brian Schlenker <bschlenker@figma.com>
2024-09-20 12:09:36 -0400

    upgrade to modules and just open the html file in the browser

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-const fs = require('fs');
-const path = require('path');
-const nunjucks = require('nunjucks');
-const dateFilter = require('nunjucks-date-filter');
-const pdf = require('html-pdf');
-const resume = require('./resume.json');
+import fs from 'node:fs'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url';
+import { execFileSync } from 'node:child_process'
 
-const TEMPLATE_DIR = path.resolve(__dirname, 'templates');
-const OUT_DIR = path.resolve(__dirname, '..', 'dist');
-const OUT_PDF = path.resolve(OUT_DIR, 'resume.pdf');
-const OUT_HTML = path.resolve(OUT_DIR, 'resume.html');
+import nunjucks from 'nunjucks'
+import dateFilter from 'nunjucks-date-filter'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const RESUME = path.join(__dirname, './resume.json')
+const TEMPLATE_DIR = path.resolve(__dirname, 'templates')
+const OUT_DIR = path.resolve(__dirname, '..', 'dist')
+const OUT_PDF = path.resolve(OUT_DIR, 'resume.pdf')
+const OUT_HTML = path.resolve(OUT_DIR, 'resume.html')
+
+const resume = JSON.parse(fs.readFileSync(RESUME, 'utf8'))
 
 const theme = {
   styles: [path.resolve(__dirname, 'style.css')],
-};
+}
 
-const env = nunjucks.configure(TEMPLATE_DIR, { trimBlocks: true });
-env.addFilter('date', dateFilter);
-env.addGlobal('includeFile', src => fs.readFileSync(src));
+const env = nunjucks.configure(TEMPLATE_DIR, { trimBlocks: true })
+env.addFilter('date', dateFilter)
+env.addGlobal('includeFile', src => fs.readFileSync(src))
 
-const html = env.render('index.html', { ...resume, theme });
-fs.mkdirSync(OUT_DIR, { recursive: true });
-fs.writeFileSync(OUT_HTML, html);
-pdf.create(html, {
-  format: 'Letter',
-}).toFile(OUT_PDF, (err) => {
-  if (err) {
-    console.error(err);
-  }
-});
+const html = env.render('index.html', { ...resume, theme })
+fs.mkdirSync(OUT_DIR, { recursive: true })
+fs.writeFileSync(OUT_HTML, html)
+
+execFileSync('open', [OUT_HTML])
+
